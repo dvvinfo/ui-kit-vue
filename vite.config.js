f93f93c817fbd6ceb74b7aebfff1af782ccc94a1
dvvinfo <dvvinfo@mail.ru>
2022-12-28 16:28:51 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://dvvinfo.github.io/ui-kit-vue/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
